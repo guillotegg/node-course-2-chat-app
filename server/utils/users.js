7d83761d8c30cb493d5ad4247a3f7f824bded658
Guille <guillote_gg@hotmail.com>
2018-05-25 00:16:50 -0300

    Send messages to only people en same room

--- server/utils/users.js
+++ server/utils/users.js
@@ -16,7 +16,7 @@ class Users {
     }
 
     getUser(id) {
-        return this.users.filter((user) => user.id === id);
+        return this.users.filter((user) => user.id === id)[0];
     }
 
     getUserList(room) {
